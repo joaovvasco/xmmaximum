232a5528981644f2d2a3ad4e75c56033a21ab55b
João Vasco <jovicto1990@gmail.com>
2025-10-05 13:43:30 -0300

    melhoria process-build e adicionando sitemap.xml

--- process-build.js
+++ process-build.js
@@ -1,21 +1,29 @@
 import fs from 'fs';
 import { execSync } from 'child_process';
 
-const isDev = process.argv.slice(2).includes('dev');
-const robotsTag = isDev
+const args = process.argv.slice(2);
+const env = args !== null && args.length > 0? args[0] : '';  
+console.log(env)
+const robotsTag = env === 'dev'
   ? '<meta name="robots" content="noindex">'
   : '';
-
+console.log(robotsTag)
 const indexPath = './dist/index.html';
 
 try {
   console.log(`Iniciando build...`);
-  execSync(`vite build ${!isDev? '&& rm dist/robots.txt' : ''}`, { stdio: 'inherit' });
+
+  let command = `vite`;
+
+  if(env !== 'local')
+    command += ` build ${env == 'prod'? '&& rm dist/robots.txt' : '&& rm dist/sitemap.xml'}`;
+
+  execSync(command, { stdio: 'inherit' });
   
   let indexContent = fs.readFileSync(indexPath, 'utf8');
   const updatedContent = 
   indexContent.replace('%REACT_APP_ROBOTS_META%', robotsTag)
-                .replace('%ENV%', isDev ? 'DEV - ' : '');
+                .replace('%ENV%', env === 'dev' ? 'DEV - ' : '');
   fs.writeFileSync(indexPath, updatedContent, 'utf8');
 } catch (error) {
   console.error('Build failed:', error);
