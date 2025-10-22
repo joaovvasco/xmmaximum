94b9b574d587717297feb671b7a7bcc7df0d35f4
joaovvasco <56969132+joaovvasco@users.noreply.github.com>
2025-10-22 22:28:27 +0000

    Ids para o acionamento GTM

--- src/components/buttons/SocialButton.tsx
+++ src/components/buttons/SocialButton.tsx
@@ -14,9 +14,9 @@ interface ISocialMedia{
     image: string
 }
 
-const SocialButton: React.FC<ISocialMedia & ISocialMediaInfo & React.HTMLAttributes<HTMLElement>> = ({image, alt, href, style }) => {
+const SocialButton: React.FC<ISocialMedia & ISocialMediaInfo & React.HTMLAttributes<HTMLElement>> = ({image, alt, href, style, id }) => {
     return <>
-        <a href={href} target='_blank' className={'button'} style={style}>
+        <a id={id} href={href} target='_blank' className={'button'} style={style}>
             <img src={image} alt={alt} />
         </a>
     </>
@@ -31,17 +31,19 @@ interface IWhatsappMessage{
     message?: string
 }
 
-export const WhatsappButton:React.FC<IContact & IWhatsappMessage & React.HTMLAttributes<HTMLElement>> = ({ contact, name, message}) => {
+export const WhatsappButton:React.FC<IContact & IWhatsappMessage & React.HTMLAttributes<HTMLElement>> = ({ contact, name, message, id}) => {
     
     return <SocialButton
+        id={id? id : "whatsapp"}
         image={whatsapp}
         href={`https://wa.me/${contact + (message ? `?text=${encodeURIComponent(message)}` : '')}`}
         alt={name}
     />
 }
 
-export const InstagramButton: React.FC<IContact & React.HTMLAttributes<HTMLElement>> = ({ contact, name, style}) => {
+export const InstagramButton: React.FC<IContact & React.HTMLAttributes<HTMLElement>> = ({ contact, name, style, id}) => {
     return <SocialButton
+        id={id? id : "instagram"}    
         image={instagram}
         href={`https://www.instagram.com/${contact}`}
         alt= {name}
@@ -49,8 +51,9 @@ export const InstagramButton: React.FC<IContact & React.HTMLAttributes<HTMLEleme
     />
 }
 
-export const YoutubeButton: React.FC<IContact & React.HTMLAttributes<HTMLElement>> = ({ contact, name, style}) => {
+export const YoutubeButton: React.FC<IContact & React.HTMLAttributes<HTMLElement>> = ({ contact, name, style, id}) => {
     return <SocialButton
+        id={id ? id : "youtube"}
         image={youtube}
         href={`https://www.youtube.com/${contact}`}
         alt= {name}
